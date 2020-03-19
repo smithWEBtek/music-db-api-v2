2d2250c7badb963191fec99106fb688e271b5531
Brad Smith <brad@smithwebtek.com>
2020-03-19 17:00:42 -0400

    Update capfile lock version to 3.12.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+lock "~> 3.12.1"
 
 set :application, "musicdb"
 set :repo_url, "git@github.com:smithwebtek/music-db-api-v2.git"
