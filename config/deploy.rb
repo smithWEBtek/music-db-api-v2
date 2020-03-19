3de0f90fcfcf3a5f0e1749d00a8171460b954bea
Brad Smith <brad@smithwebtek.com>
2020-03-19 12:14:59 -0400

    set ssh_options

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,5 +49,4 @@ set :rails_assets_groups, :assets
 set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
-
+set :ssh_options, verify_host_key: :secure
