da9982033daeead30ab493a25d84895ad7531ced
Brad Smith <brad@smithwebtek.com>
2020-03-19 14:28:29 -0400

    add role web

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "mdb.smithpiano.com", user: "deploy", roles: %w{app db}
-server "206.189.186.90", user: "deploy", roles: %w{app db}
+server "206.189.186.90", user: "deploy", roles: %w{app web db}
 
 
 # role-based syntax
