3e77016be93d3328e4470ae094c43f7b3476ccd0
Brad Smith <brad@smithwebtek.com>
2020-03-19 16:05:48 -0400

    Remove master.key from linked_files list

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,8 @@ set :rvm_ruby_version, '2.6.3'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key"
+# append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
