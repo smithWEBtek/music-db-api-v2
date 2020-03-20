83d71b8f08a61e7f99dcf08d16e737877df214b7
Brad Smith <brad@smithwebtek.com>
2020-03-20 13:13:11 -0400

    Troubleshoot yarn

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,3 +53,5 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :format, :airbrussh
