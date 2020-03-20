cb98be80ea209fcbed3d1a4c432c946aa9d4c8d9
Brad Smith <brad@smithwebtek.com>
2020-03-20 14:18:22 -0400

    Pty tru

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :rvm_ruby_version, '2.6.5'
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key"
