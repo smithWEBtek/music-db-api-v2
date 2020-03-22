c6eb397fc71381dbd1e8873ff5966ddb777eec37
github username <github email address>
2020-03-22 10:19:45 -0400

    Run bundle, update lock version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 # config valid only for Capistrano 3.1
-# lock '3.2.1'
-lock '3.12.1'
+lock '3.2.1'
 
 set :application, "musicdb"
 set :repo_url, "git@github.com:smithwebtek/music-db-api-v2.git"
@@ -9,12 +8,13 @@ set :rvm_ruby_version, '2.6.5'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/deploy/musicdb'
 
 # Default value for :format is :pretty
-set :format, :pretty
+# set :format, :pretty
 
 # Default value for :log_level is :debug
 set :log_level, :debug
@@ -30,10 +30,10 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 
 # Defaults to ["/path/to/release_path/public/#{fetch(:assets_prefix)}/.sprockets-manifest*", "/path/to/release_path/public/#{fetch(:assets_prefix)}/manifest*.*"]
 # This should match config.assets.manifest in your rails config/application.rb
-set :assets_manifests, ['app/assets/config/manifest.js']
+# set :assets_manifests, ['app/assets/config/manifest.js']
 
 # RAILS_GROUPS env value for the assets:precompile task. Default to nil.
-set :rails_assets_groups, :assets
+# set :rails_assets_groups, :assets
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -59,6 +59,19 @@ namespace :deploy do
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
+    end
+  end
+
+  before "deploy:assets:precompile", "deploy:npm_install"
+
+  namespace :deploy do
+    desc 'Run rake npm:install'
+    task :npm_install do
+      on roles(:web) do
+        within release_path do
+          execute("cd #{release_path} && npm install")
+        end
+      end
     end
   end
 
