1e22276e27fb5ae986a55d19c2d5caf875540949
github username <github email address>
2020-03-22 09:24:29 -0400

    Remove lines from production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,11 +4,6 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
-
-
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server definition into the
