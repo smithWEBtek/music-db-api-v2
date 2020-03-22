abd362f061138c84a75d31da3f3d2635fff9fa5c
github username <github email address>
2020-03-22 09:22:40 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,9 +13,6 @@ set :rvm_ruby_version, '2.6.5'
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/deploy/musicdb'
 
-# Default value for :scm is :git
-set :scm, :git
-
 # Default value for :format is :pretty
 set :format, :pretty
 
